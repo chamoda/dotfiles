09061fa90b07e5abf9ad5ac186d9098fbd85eb1c
Chamoda Pandithage <chamoda@xaventra.com>
2024-09-21 10:08:09 +0530

    Update layout

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sleep 10
+sleep 6
 
-i3-msg "workspace 2; append_layout ~/.config/i3/layout-2.json"
-sleep 1
-i3-msg "workspace 1; append_layout ~/.config/i3/layout-1.json"
-i3-msg "workspace 10; exec gnome-pomodoro"
+i3-msg "workspace 10; append_layout ~/.config/i3/layouts/layout-10.json"
+i3-msg "workspace 2; append_layout ~/.config/i3/layouts/layout-2.json"
+i3-msg "workspace 1; append_layout ~/.config/i3/layouts/layout-1.json"
 
+(thunderbird &)
 (firefox &)
 (emacs ~/org/tasks.org &)
 (i3-sensible-terminal &)
-(thunderbird &)
+(gnome-pomodoro &)
+
+
