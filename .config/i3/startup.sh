4fde367f156dca34bb6f24d1eda3f185c62cafa2
Chamoda Pandithage <chamoda@xaventra.com>
2024-09-21 09:29:23 +0530

    Add gnome-pomodoro to startup

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sleep 15
+sleep 10
 
 i3-msg "workspace 2; append_layout ~/.config/i3/layout-2.json"
 sleep 1
 i3-msg "workspace 1; append_layout ~/.config/i3/layout-1.json"
+i3-msg "workspace 10; exec gnome-pomodoro"
 
 (firefox &)
 (emacs ~/org/tasks.org &)
