e14e004c70ebbdcb30bd0c35759d57282b3e3727
Chamoda Pandithage <chamoda@xaventra.com>
2024-10-16 11:58:41 +0530

    Add slack to startup layout, improved docs on adding layouts

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -4,6 +4,7 @@ sleep 6
 
 i3-msg "workspace 10; append_layout ~/.config/i3/layouts/layout-10.json"
 i3-msg "workspace 2; append_layout ~/.config/i3/layouts/layout-2.json"
+i3-msg "workspace 3; append_layout ~/.config/i3/layouts/layout-3.json"
 i3-msg "workspace 1; append_layout ~/.config/i3/layouts/layout-1.json"
 
 (thunderbird &)
@@ -11,5 +12,6 @@ i3-msg "workspace 1; append_layout ~/.config/i3/layouts/layout-1.json"
 (emacs ~/org/tasks.org &)
 (i3-sensible-terminal &)
 (gnome-pomodoro &)
+(slack &)
 
 
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-Go to repo directry and type
+Go to repository directory and type
 
 `stow .`
 
 `cp logid.cfg /etc/logid.cfg`
+
+# i3
+
+## Save layouts and load on startup.
+
+* Run `i3-save-tree --workspace NUM > ~/dotfiles/.config/i3/layouts/layout-NUM.json`. 
+* Edit json file to add matcher (in swallows section).
+* Add the relavent programs in `.config/i3/startup.sh`
+
+
+
 
